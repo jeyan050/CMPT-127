55f42fe1698c93958ef731d756408b84476cddb4
jeyan <justin_yan_2@sfu.ca>
2020-01-21 13:13:05 -0800

    t2 submission

---
+++ 1/t2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <limits.h>
+ 
+int main( void ) 
+{
+	int charBits = sizeof(char)  * CHAR_BIT;
+	int unSignCharBits = sizeof(unsigned char)  * CHAR_BIT;
+	int intBits = sizeof(int)  * CHAR_BIT;
+	int unSignIntBits = sizeof(unsigned int)  * CHAR_BIT;
+	int longBits = sizeof(long)  * CHAR_BIT;
+	int unSignLongBits = sizeof(unsigned long)  * CHAR_BIT;
+	int floatBits = sizeof(float)  * CHAR_BIT;
+	int doubleBits = sizeof(double)  * CHAR_BIT;
+	int longDoubleBits = sizeof(long double)  * CHAR_BIT;
+
+	printf("%d %d %d %d %d %d %d %d %d\n", charBits, unSignCharBits, intBits, unSignIntBits, longBits, unSignLongBits, floatBits, doubleBits, longDoubleBits);
+}

19cf3831f12578eb876c5994132926f5babdd134
jeyan <justin_yan_2@sfu.ca>
2020-02-03 21:42:17 -0800

    t9 submission

---
+++ 1/t9.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h> 
+
+int main(){
+  int w, l, h;
+  scanf("%d %d %d", &w, &l, &h);
+
+  // TOP AND BODY
+  int dx = w/2;
+  int dz = h;
+  
+  int j = 3;
+  // FIRST # and SPACES
+  for (int level = h; level > 0; level--){
+    int x = dx * (level) / dz;
+    for (int a = x; a > 1; a--){
+      printf(" ");      
+    }
+    printf("#");   
+  // DOTS AND LEFTOVER HASHES
+    int left = floor( j * (w/(2.0*h)) ); // 0 0 1 2
+    int right = ceil( (w-1) - (j *(w/(2.0*h))) ); // 6 6 5 4
+    int differ = right - left;
+
+    if (level == 1 || level == h){
+      for (int hash = 0; hash < differ; hash++){
+        printf("#");
+      }
+      printf("\n");
+    } else {
+      for (int dot = 1; dot < differ; dot++){
+        printf(".");
+      }
+      printf("#\n");
+    } 
+    j--;        
+  }
+}

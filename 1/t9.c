13e6b2d54940f7756d810ae540e2ece1681001f4
jeyan <justin_yan_2@sfu.ca>
2020-02-04 00:26:37 -0800

    t9 submission

--- 1/t9.c
+++ 1/t9.c
@@ -11,16 +11,17 @@ int main(){
   
   int j = h - 1;
   // FIRST # and SPACES
-  for (int level = h; level > 0; level--){
-    int x = dx * (level) / dz;
+  for (int level = h; level > 0; level--){ // 4 3 2 1
+    double m = dx * level;
+    int x = ceil(m / dz);
     for (int a = x; a > 1; a--){
       printf(" ");      
     }
     printf("#");   
   // DOTS AND LEFTOVER HASHES
-    int left = floor( j * (w/(2.0*h)) ); // 0 5
-    int right = ceil( (w-1) - (j *(w/(2.0*h))) ); // 11 6
-    int differ = right - left; // 11 1
+    int left = floor( j * (w/(2.0*h)) ); // 0 1/2 1 3/2   -- 0 0 1 1
+    int right = ceil( (w-1) - (j *(w/(2.0*h))) ); // 3 3 2 2
+    int differ = right - left; // 3 3 1 1
 
     if (level == 1 || level == h){
       for (int hash = 0; hash < differ; hash++){

ad7f1c3b5bf508bfd91c5f2e380f74454066c76b
jeyan <justin_yan_2@sfu.ca>
2020-02-10 11:07:14 -0800

    identical is working

--- 2/identical.c
+++ 2/identical.c
@@ -1,14 +1,18 @@
 int identical( int arr1[], int arr2[], unsigned int len ){
-	int intentical;
-	
-	for (int i = 0; i< len; i++){
-		if (arr1[i] == arr2[i]){
-			return 1;		
-		} else {
-			return 0;
-		}
+	int numSame = 0;
+
+	if (len == 0){
+		return 1;	
 	}
-
+	for (int i = 0; i < len; i++){
+		if (arr1[i] == arr2[i]){
+			numSame++;	
+		} 
+	}
+	if (numSame != len){
+		return 0;	
+	} else {
+		return 1;		
+	}
 }
-
 

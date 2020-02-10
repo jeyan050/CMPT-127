100f32d324bd6de005908dd93f5dbc0bf5c73b3d
jeyan <justin_yan_2@sfu.ca>
2020-02-10 10:45:57 -0800

    identical is working

--- 2/identical.c
+++ 2/identical.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 int identical( int arr1[], int arr2[], unsigned int len ){
 	int intentical;
 	
@@ -13,18 +11,4 @@ int identical( int arr1[], int arr2[], unsigned int len ){
 
 }
 
-int main( void )
-{
-   int arr1[3] = {10, 15, 20};
-   int arr2[3] = {10, 20, 15};
- 
-   if( identical( arr1, arr2, 3 ) == 1 )
-   { 
-      printf( "arr1 and arr2 are identical\n" );
-   }  else {
-      printf( "arr1 and arr2 are NOT identical\n" );
-   }
- 
-   return 0;
-}
 

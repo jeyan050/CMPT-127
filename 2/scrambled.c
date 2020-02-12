f6478944821bb53b2e5c9a3faa7170e05dcdfa57
jeyan <justin_yan_2@sfu.ca>
2020-02-12 10:41:18 -0800

    scrambled is working

--- 2/scrambled.c
+++ 2/scrambled.c
@@ -1,16 +1,23 @@
 int scrambled( unsigned int arr1[], unsigned int arr2[], unsigned int len ){
-  int a = 0;
-  int n = 0;
-  int sameNums = 0;
-  while (n < len){
-    if (arr1[n] == arr2[a]){
-      sameNums++;
-      n++;
-    } else {
-      a++;
+  int totalSame1 = 0;
+  int totalSame2 = 0;
+  for (int i = 0; i < len; i++){
+    for (int a = 0; a < len; a++){
+      if (arr1[i]==arr2[a]){
+        totalSame1++;
+	break;
+      }
     }
   }
-  if (sameNums == len){
+  for (int i = 0; i < len; i++){
+    for (int a = 0; a < len; a++){
+      if (arr1[i]==arr2[a]){
+        totalSame2++;
+	break;
+      }
+    }
+  }
+  if (totalSame1 == totalSame2){
     return 1;
   } else {
     return 0;

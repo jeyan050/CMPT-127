6fd3c93f94f9ca5d0c603b1dcb81ab873445e7a4
jeyan <justin_yan_2@sfu.ca>
2020-02-12 22:54:58 -0800

    scrambled is working

--- 2/scrambled.c
+++ 2/scrambled.c
@@ -1,18 +1,20 @@
 int scrambled( unsigned int arr1[], unsigned int arr2[], unsigned int len ){
-  int product1 = 0;
-  int product2 = 0; 
-  int remain1 = 0;
-  int remain2 = 0;
+  int productArr1 = 1;
+  int productArr2 = 1;
+
+  int sumArr1 = 0;
+  int sumArr2 = 0;
 
   for (int i = 0; i < len; i++){
-    product1*= arr1[i];
-    product2*= arr2[i];
-    
-    product1/= arr2[i];
-    product2/= arr1[i];
+
+    sumArr1 += arr1[i];
+    sumArr2 += arr2[i];
+
+    productArr1 *= arr1[i];
+    productArr2 *= arr2[i];
   }
 
-  if (product2 == 0 && product1 == 0){
+  if (productArr1 == productArr2 && sumArr1 == sumArr2){
     return 1;
   } else {
     return 0;

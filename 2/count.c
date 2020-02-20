ff16ea52355525ddec35a0a6f2ef0e13258c5480
jeyan <justin_yan_2@sfu.ca>
2020-02-20 01:06:26 -0800

    count is working

--- 2/count.c
+++ 2/count.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
 
+
+
 int main (){
+
 	char ch;
-	unsigned int charcount = 0;
-	unsigned int wordcount = 0;
-	unsigned int linecount = 0;
+
+	long unsigned int charcount = 0;
+
+	long unsigned int wordcount = 0;
+
+	long unsigned int linecount = 0;
+	int word = 1;
+
 	
+
 	while((ch = getchar()) != EOF){
-		charcount+= 1;	
-		if (ch == ' '||ch == '\n'){
-			wordcount+= 1;
-			if (ch == '\n'){
-				linecount+= 1;	
-			}
+
+		charcount+= 1;
+		if (ch>='a' && ch<='z'||ch>='A' && ch<='Z'||ch=='\''){
+			if (word==1){
+    				wordcount+=1;
+    				word=0;
+    			}	
+		} else {
+			word=1;
 		}
+
+		if (ch == '\n'){
+
+			linecount+= 1;
+			word=1;	
+
+		}
+
 	}
+
 	printf( "%lu %lu %lu\n", charcount, wordcount, linecount ); 
+
 	return 0; 	
+
 }
